ca97b86ff04f345b6a48720a17bcc56bed9e88e4
Cristian Vanegas <cristian.vanegas@newshore.es>
2023-08-22 10:35:38 -0500

    Duplicar-Triplicar-Reiniciar Número

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,5 +6,19 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'bases';
+  title: string = 'Primera Semana:';
+
+  numero: number = 1;
+
+  duplicar(){
+    this.numero = this.numero*2;
+  }
+
+  triplicar(){
+    this.numero = this.numero*3;
+  }
+
+  reiniciar(){
+    this.numero = 1;
+  }
 }
